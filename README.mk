63fa22dfa30310b3d5a1057ee4e676762157265a
Micah Wylde <mwylde@wesleyan.edu>
2010-12-24 23:42:52 -0800

    Made README more informative.

---
+++ README.mk
@@ -0,0 +1,6 @@
+# Resume.rb
+
+Resume.rb provides a DSL for creating resumes, with flexible
+output. Currently supported is PDF (via LaTeX), but Markdown and HTML
+backends are planned. Look in examples/ for a full example, which
+generates [this pdf](https://github.com/mwylde/resume.rb/tree/master/example/my_resume.pdf).
